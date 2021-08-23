3b452e7da2c31d610d9ee10515d234fc7ca08ac5
unknown <lucas.bruneli.ramos@gmail.com>
2021-08-22 22:28:03 -0300

    training project buy list

---
+++ buyList/script.js
@@ -0,0 +1,27 @@
+// - Exibir modal Register / Cheking
+
+const Modal = {
+
+  openRegister () {
+     document.querySelector('.modal-register').classList.add('active')
+  },
+
+  closeRegister () {
+    document.querySelector('.modal-register').classList.remove('active')
+  },
+
+  openCheking () {
+    document.querySelector('.modal-cheking').classList.add('active')
+  },
+
+  closeCheking () {
+    document.querySelector('.modal-cheking').classList.remove('active')
+  }
+
+}
+
+const Dom = {
+
+  
+
+}
